8df79ab6557e675711cbf77b98997297bc183f13
Chimaxs <chimaxsbb2@gmail.com>
2023-02-16 12:50:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return: 0 (Success)
+ * Return: 0 (SUCCESS)
  */
 int main(void)
 {
@@ -9,3 +9,4 @@ puts("\"Programming is like building a multilingual puzzle
 ");
 return (0);
 }
+
